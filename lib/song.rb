4b8472484b0da576de039b982ca21cf8e8185036
github username <github email address>
2015-10-14 16:52:39 -0400

    files

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,22 @@
+require "tempfile"
+require "pry"
 class Song
   # code goes here
+  attr_accessor :title, :artist
+  def initialize
+    @title = ""
+    @artist = ""
+  end
+
+  def artist=(new_artist)
+    @artist = new_artist
+  end
+
+  def slugify
+    file_name = @title.downcase.gsub(" ", "_")
+    content = "#{@artist.name} - #{@title}"
+    file = Tempfile.new([file_name, ".txt"], "tmp")
+    file.write(content)
+    file.close
+  end
 end
